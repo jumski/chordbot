c954fd76bb95f366fbd1f769cce6bd21d3ab985a
Wojtek Majewski <jumski@gmail.com>
2017-03-03 21:41:30 +0100

    Simpler component working

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 import styles from './stylesheets/application.css';
 import pianoStyles from './stylesheets/piano.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Piano from './components/piano';
+
+ReactDOM.render(
+  <Piano/>,
+  document.getElementById('app')
+);
---
+++ src/components/piano.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Piano extends Component {
+  render() {
+    return <div>Omg hax!</div>;
+  }
+}
