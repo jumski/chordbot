f9ba907f685e1d16bb36659f6f294bf74435bd06
Wojtek Majewski <jumski@gmail.com>
2017-03-09 14:26:26 +0100

    Add standardjs

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,22 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        // set up standard-loader as a preloader
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        loader: 'standard-loader',
+        exclude: /(node_modules|bower_components)/,
+        // options: {
+        //   // Emit errors instead of warnings (default = false)
+        //   error: false,
+        //   // enable snazzy output (default = true)
+        //   snazzy: true,
+        //   // other config options to be passed through to standard e.g.
+        //   parser: 'babel-eslint'
+        // }
+      },
+      {
+        test: /\.jsx?$/,
         exclude: [/node_modules/],
         use: [{
           loader: 'babel-loader',
